88301688851585970325b36953bf8a9667797dad
JimmmChen <jingchen9412@gmail.com>
2018-04-15 15:48:18 -0400

    jc

--- views/footer.php
+++ views/footer.php
@@ -2,16 +2,16 @@
 <footer class="footer" >
 
     <div class="container" align="center">
-        
+
         <p>&copy;BookStore infsci2710</p>
-    
+
     </div>
 
 </footer>
 </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
@@ -51,27 +51,27 @@
 <script>
 
     $("#toggleLogin").click(function() {
-        
+
         if ($("#loginActive").val() == "1") {
-            
+
             $("#loginActive").val("0");
             $("#loginModalTitle").html("Sign Up");
             $("#loginSignupButton").html("Sign Up");
             $("#toggleLogin").html("Login");
-            
-            
+
+
         } else {
-            
+
             $("#loginActive").val("1");
             $("#loginModalTitle").html("Login");
             $("#loginSignupButton").html("Login");
             $("#toggleLogin").html("Sign up");
-            
+
         }
-        
-        
+
+
     })
-    
+
     $("#loginSignupButton").click(function() {
         $.ajax({
             type: "POST",
@@ -79,45 +79,45 @@
             data: "email=" + $("#email").val() + "&password=" + $("#password").val() + "&loginActive=" + $("#loginActive").val(),
             success: function(result) {
                 if (result == "1") {
-                    
-                    window.location.assign("http://localhost/onlinebookstore/");
-                    
+
+                    window.location.assign("./");
+
                 } else {
-                    
+
                     $("#loginAlert").html(result).show();
-                    
+
                 }
             }
-            
+
         })
-        
+
     })
 
     $(".toggleFollow").click(function() {
-        
+
         var id = $(this).attr("data-userId");
-        
+
         $.ajax({
             type: "POST",
             url: "actions.php?action=toggleFollow",
             data: "userId=" + id,
             success: function(result) {
-                
+
                 if (result == "1") {
-                    
+
                     $("a[data-userId='" + id + "']").html("Follow");
-                    
+
                 } else if (result == "2") {
-                    
+
                     $("a[data-userId='" + id + "']").html("Unfollow");
-                    
+
                 }
             }
-            
+
         })
-        
+
     })
-    
+
 </script>
 
 
